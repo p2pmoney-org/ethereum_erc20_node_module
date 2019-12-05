72a69690dbdcb7813c88edeba79f3c53d66dc3bd
fbaud <fbaud@p2pventure.org>
2019-12-05 12:30:16 +0100

    version 0.13.1

--- imports/dapps/module.js
+++ imports/dapps/module.js
@@ -4,7 +4,7 @@ var Module = class {
 	
 	constructor() {
 		this.name = 'dapps';
-		this.current_version = "0.13.0.2019.12.05";
+		this.current_version = "0.13.1.2019.12.05";
 		
 		this.global = null; // put by global on registration
 		this.isready = false;
