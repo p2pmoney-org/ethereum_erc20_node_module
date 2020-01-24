6e40046b72ab1115edd4f159f96b97626850ff17
fbaud <fbaud@p2pventure.org>
2020-01-24 11:33:37 +0100

    version 0.14.2

--- imports/dapps/erc20/includes/model/interface/erc20token-localpersistor.js
+++ imports/dapps/erc20/includes/model/interface/erc20token-localpersistor.js
@@ -6,33 +6,60 @@ var ERC20TokenLocalPersistor = class {
 		this.session = session;
 		this.contractuuid = contractuuid;
 		
-		this.commonmodule = this.session.getGlobalObject().getModuleObject('common');
+		this.global = this.session.getGlobalObject()
+		this.commonmodule = this.global.getModuleObject('common');
 	}
 	
 	saveERC20TokenJson(erc20token, callback) {
+		var global = this.global;
 		var session = this.session;
-		var keys = ['contracts'];
 		
 		var uuid = erc20token.getUUID();
 		var json = erc20token.getLocalJson();
 		
 		console.log('ERC20TokenLocalPersistor.saveERC20TokenJson json to save is ' + JSON.stringify(json));
 		
-		// update cache
 		var commonmodule = this.commonmodule;
+		var ethnodemodule = global.getModuleObject('ethnode');
+
+		// refresh cache before inserting leave to be sure not to overload with out of sync cache
+		return new Promise((resolve, reject) => { 
+			ethnodemodule.getContractsObject(session, true, (err, res) => {
+				if (err) reject(err); else resolve(res);
+			});
+		})
+		.then(() => {
+			// update cache
+			var keys = ['contracts'];
+			
+			var jsonleaf = commonmodule.getLocalJsonLeaf(session, keys, uuid);
+			if (jsonleaf) {
+				commonmodule.updateLocalJsonLeaf(session, keys, uuid, json);
+			}
+			else {
+				commonmodule.insertLocalJsonLeaf(session, keys, null, null, json);
+			}
+			
+			// save contracts in cache under keys
+			var contractsjson = commonmodule.readLocalJson(session, keys); // from cache, since no refresh
+			
+			return new Promise((resolve, reject) => { 
+				commonmodule.saveLocalJson(session, keys, contractsjson, (err, res) => {
+					if (err) reject(err); else resolve(res);
+				})
+			});
+		})
+		.then(() => {
+			if (callback)
+				callback(null, json)
+		})
+		.catch(err => {
+			console.log('error in ERC20TokenLocalPersistor.saveERC20TokenJson: ' + err);
+			
+			if (callback)
+				callback(err, null)
+		});
 		
-		var jsonleaf = commonmodule.getLocalJsonLeaf(session, keys, uuid);
-		if (jsonleaf) {
-			commonmodule.updateLocalJsonLeaf(session, keys, uuid, json);
-		}
-		else {
-			commonmodule.insertLocalJsonLeaf(session, keys, null, null, json);
-		}
-		
-		// save contracts
-		var contractsjson = commonmodule.readLocalJson(session, keys); // from cache, since no refresh
-		
-		commonmodule.saveLocalJson(session, keys, contractsjson, callback);
 	}
 	
 }
